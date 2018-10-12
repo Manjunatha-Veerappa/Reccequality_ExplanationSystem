43e0cb4eb8af794028b6cbb60d03326abd0dc1bd
Manjunatha Veerappa <manjunatha.veerappa@iosb.fraunhofer.de>
2018-10-12 15:25:16 +0200

    data display on dashboard using pie slices for Landfahrzeug and Schiffe

--- frontend/static/dashboard_js_files/schiffe/datasetPieChartJS.js
+++ frontend/static/dashboard_js_files/schiffe/datasetPieChartJS.js
@@ -48,7 +48,89 @@ let options = {
               return percentage + "% - " + currentValue + " vectors";
             }
          }
-     }
+    },
+    onClick:function(e){
+        var activePoints = pieChartObj.getElementsAtEvent(e);
+        var selectedIndex = activePoints[0]._index;
+        if(selectedIndex == 0){
+            console.log('Good data has been selected')
+            $.ajax({
+               url:"/static/classification_csv_files/SchiffeClassificationCategorical.csv",
+               dataType:"text",
+               success:function(data)
+               {
+                var resultCount = 0;
+                var classification_data = data.split(/\r?\n|\r/);
+                var table_data = '<table class="table table-bordered table-striped">';
+                for(var count = 0; count<classification_data.length; count++)
+                {
+                 var cell_data = classification_data[count].split(",");
+                 table_data += '<tr>';
+                 for(var cell_count=0; cell_count<cell_data.length; cell_count++)
+                 {
+                  if(count === 0)
+                  {
+                   table_data += '<th>'+cell_data[cell_count]+'</th>';
+                  }
+                  else
+                  {
+                    if(cell_data[cell_data.length - 1] == 1){
+                        //cell_data[cell_count].cells[0].backgroundColor = "#c1c1c1";
+                        table_data += '<td>'+cell_data[cell_count]+'</td>';
+                    }
+                  }
+                 }
+                 table_data += '</tr>';
+                }
+                table_data += '</table>';
+                $('#data_table').html(table_data);
+               }
+
+            });
+        }
+        else{
+            console.log('Bad data has been selected')
+            $.ajax({
+               url:"/static/classification_csv_files/LandfahrzeugClassificationCategorical.csv",
+               dataType:"text",
+               success:function(data)
+               {
+                var resultCount = 0;
+                var classification_data = data.split(/\r?\n|\r/);
+                var table_data = '<table class="table table-bordered table-striped">';
+                for(var count = 0; count<classification_data.length; count++)
+                {
+                 var cell_data = classification_data[count].split(",");
+                 table_data += '<tr>';
+                 for(var cell_count=0; cell_count<cell_data.length; cell_count++)
+                 {
+                  if(count === 0)
+                  {
+                   table_data += '<th>'+cell_data[cell_count]+'</th>';
+                  }
+                  else
+                  {
+                    if(cell_data[cell_data.length - 1] == 0){
+                        //Abmessungen Länge exception
+                        if(cell_data[1] == -1 && cell_count == 1){
+                            table_data += '<td bgcolor="#FA8072">'+cell_data[cell_count]+'</td>';
+                        }
+
+                        else{
+                            table_data += '<td>'+cell_data[cell_count]+'</td>';
+                        }
+                    }
+                  }
+                 }
+                 table_data += '</tr>';
+                }
+                table_data += '</table>';
+                $('#data_table').html(table_data);
+               }
+
+            });
+        }
+    }
 };
 
 let pieChartObj = new Chart(pieChart, {
